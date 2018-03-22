9a79861d9318cf723895ecd01758f7071cfe379c
Anders <andersogranaas@gmail.com>
2018-03-22 20:50:23 +0100

    Some formatting

--- src/project/DBConn.java
+++ src/project/DBConn.java
@@ -2,23 +2,17 @@ package project;
 import java.sql.*;
 public class DBConn {
 
-	
 	public static Connection conn = null;
 	
 	public static void connectDB() {
-
 		try {
-			
 			Class.forName("com.mysql.jdbc.Driver").newInstance();
-			
-			conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/setup?autoReconnect=true&useSSL=false","root","root");
-			
-
+			conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/setup"
+											+ "?autoReconnect=true&useSSL=false","root","root");
 		}
 		catch (Exception exc) {
 			exc.printStackTrace();
-		}
-		
+		}	
 	}
 	
 	public static Connection getConnection() {
